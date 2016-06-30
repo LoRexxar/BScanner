c6456d00a520d3c7bb3a1e0279679ed6c9f921b9
LoRexxar <guoyingqi001@126.com>
2016-06-30 22:16:35 +0800

    fix some log

--- lib/wordbuild.py
+++ lib/wordbuild.py
@@ -12,6 +12,8 @@ def build_wordlist(wordlist_file):
     logger.info("start wordlist build...")
     raw_words = fd.readlines()
     logger.info("This dictionary contains %s rows" % len(fd.readlines()))
+    if len(fd.readlines()) == 0:
+        logger.error("This dictionary id empty...")
     fd.close()
 
     found_resume = False
